9479fc6a371df3f7ae5f0f92c4444ccfb07ce5a7
Moamen <Moamenbekheet98@gmail.com>
2022-08-04 09:12:53 +0200

    modifications made and test passed may code might not be most efficient but it works

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(Arr, ...args) {
+    for(let i of args){
+        for(let j=0;j<=Arr.length;j++){
+            if(Arr[j]===i){
+                Arr.splice(j,1);
+            }
+        }
+    }
+    return Arr;
 };
 
 // Do not edit below this line
