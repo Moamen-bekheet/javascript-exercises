79beac9bfe5c6be8c4bbc966e1b92ed3744a8246
Moamen <Moamenbekheet98@gmail.com>
2022-08-04 06:34:31 +0200

    repeatString.js passed the test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let stringArr = [];
+    for(let i=1;i<=num;i++){
+        stringArr.push(string);
+    }
+    return stringArr.join('');
 };
 
 // Do not edit below this line
