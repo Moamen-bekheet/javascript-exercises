d6cf4eabe2ded179dd6ac434084e0c9d5c6f61ce
Moamen <Moamenbekheet98@gmail.com>
2022-08-04 10:14:56 +0200

    modifications made and test passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(integerOne,integerTwo) {
+    //tried to insert 'instanceof' to make sure integerOne and integerTwo are numbers gave me false, replaced it with typeof
+    if((typeof integerOne == 'number')&&(typeof integerTwo == 'number')&&(Math.min(integerOne,integerTwo)>=0)){
+        let sum =0;
+        if(integerTwo<integerOne){
+            for (let i=integerTwo;i<=integerOne;i++){
+                sum +=i;
+            }
+            return sum;
+        }
+        else{
+            for (let i=integerOne;i<=integerTwo;i++){
+                sum +=i;
+            }
+            return sum;
+        }
+    }
+    else{
+        return 'ERROR';
+    }
 };
-
 // Do not edit below this line
 module.exports = sumAll;
