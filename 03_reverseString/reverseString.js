aaa3a1a04d94c2acccc3ef17ad4c2c7ba6f37dd1
Moamen <Moamenbekheet98@gmail.com>
2022-08-04 07:25:35 +0200

    make modifications and passed the test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringArr = [...string];
+    let reversedStringArr = [];
+    for (let i=0; i<string.length;i++){
+        reversedStringArr.push(stringArr.pop());
+    }
+    return reversedStringArr.join('');
 };
-
 // Do not edit below this line
 module.exports = reverseString;
