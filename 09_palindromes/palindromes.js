f2f108bcae144d145ca94079ad874a8509ca715e
Moamen <Moamenbekheet98@gmail.com>
2022-08-07 08:57:45 +0200

    finished the palindrome project

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,32 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    // this part is used to get valid characters out of argument provided by the function client
+    let lowerCaseText = text.toLowerCase();
+    const prohibittedCharacters = `.,!?:;_-()[]/'" `
+    let characterArr = [];
+    for(let i=0;i<lowerCaseText.length;i++){
+        if (!(prohibittedCharacters.includes(lowerCaseText[i]))){
+           characterArr.push(lowerCaseText[i]);
+        }
+    }
+    // this part is used to get valid characters out of argument provided by the function client
+    if(characterArr.length%2===0){
+        let middle = characterArr.length/2;
+        for(let i=0; i<middle; i++){
+            if (!(characterArr[i]===characterArr[characterArr.length-(i+1)])){
+                return false;
+            }
+        }
+        return true;
+    }
+    else{
+        let middle = Math.ceil(characterArr.length/2);
+        for(let i=0; i<middle; i++){
+            if (!(characterArr[i]===characterArr[characterArr.length-(i+1)])){
+                return false;
+            }
+        }
+        return true; 
+    }
 };
 
 // Do not edit below this line
