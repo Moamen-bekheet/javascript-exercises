8bec195e29f075ad7b36dbbf61ea2adb7e645034
Moamen <Moamenbekheet98@gmail.com>
2022-08-07 10:50:40 +0200

    mission accomplished

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,21 @@
-const findTheOldest = function() {
-
-};
-
+const findTheOldest = function(peopleArr) {
+    peopleArr.sort(((personOne,personTwo) => {
+        if(!(personOne.yearOfDeath)){
+            personOne.yearOfDeath = (new Date).getUTCFullYear();
+        }
+        if(!(personTwo.yearOfDeath)){
+            personTwo.yearOfDeath = (new Date).getUTCFullYear();
+        }
+        let personOneDeathAge = personOne.yearOfDeath - personOne.yearOfBirth;
+        let personTwoDeathAge = personTwo.yearOfDeath - personTwo.yearOfBirth;
+        if(personOneDeathAge>personTwoDeathAge){
+            return -1;
+        }
+        else{
+            return 1;
+        }
+    }))
+    return peopleArr[0];
+}
 // Do not edit below this line
 module.exports = findTheOldest;
