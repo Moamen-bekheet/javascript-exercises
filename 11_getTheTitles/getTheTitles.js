b16084e0d7e7f012df451efc92b7536667e4dbb0
Moamen <Moamenbekheet98@gmail.com>
2022-08-07 10:15:28 +0200

    getTitles finished

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(booksArr) {
+    let titlesArr = []
+    for(let i=0; i<booksArr.length;i++){
+        titlesArr.push(booksArr[i].title);
+    }
+    return titlesArr;
 };
 
 // Do not edit below this line
